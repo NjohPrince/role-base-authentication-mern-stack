367ae2c8bee5c5c1a07d0629b8a8758e2cd5519a
NjohPrince <jufredprince@gmail.com>
2022-03-01 09:26:51 +0100

    bugs

--- validators/auth.js
+++ validators/auth.js
@@ -1,19 +1,19 @@
-const { check } = require("express-validator");
+const { body } = require("express-validator");
 
 exports.userSignupValidator = [
-  check("name").not().isEmpty().withMessage("Your name is required."),
-  check("name")
+  body("name").not().isEmpty().withMessage("Your name is required."),
+  body("name")
     .isLength({ min: 3 })
     .withMessage("Name must be more than 2 characters long."),
-  check("email").isEmail().withMessage("Please enter a valid email."),
-  check("password")
+  body("email").isEmail().withMessage("Please enter a valid email."),
+  body("password")
     .isLength({ min: 8 })
     .withMessage("Password must be at least 8 characters long."),
 ];
 
 exports.userSigninValidator = [
-  check("email").isEmail().withMessage("Email address invalid."),
-  check("password")
+  body("email").isEmail().withMessage("Email address invalid."),
+  body("password")
     .isLength({ min: 8 })
     .withMessage("Account passwords are usually 8+ characters long."),
 ];

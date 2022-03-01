c8111402932cf023d36652d6784865f61a6a5b24
NjohPrince <jufredprince@gmail.com>
2022-03-01 02:27:03 +0100

    connection to mongodb

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 require("dotenv").config();
+
 const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 
-const PORT = process.env.PORT || 8000;
+// setting up the database connection and server
+const startServer = async () => {
+  await mongoose
+    .connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    })
+    .then(() => {
+      console.log("Connected to MongoDB!");
+      app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+// middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+// cors
+if (process.env.NODE_ENV === "development") {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
 
 app.get("/", (req, res) => {
-    res.send({ message: " Based Authentication System - MERN Stack" }).status(200);
+  res
+    .send({ message: " Based Authentication System - MERN Stack" })
+    .status(200);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+
+startServer();
